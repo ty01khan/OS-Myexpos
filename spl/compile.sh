136dc305e91f725b31e83a4fab7af1fa72b18612
Talha Yaseen <111701019@smail.iitpkd.ac.in>
2020-02-02 19:50:57 +0530

    OS-Lab

---
+++ spl/compile.sh
@@ -0,0 +1,4 @@
+./spl $HOME/myexpos/spl/spl_progs/os_startup9.spl
+./spl $HOME/myexpos/spl/spl_progs/os_startup8.spl
+./spl $HOME/myexpos/spl/spl_progs/os_startup1.spl
+./spl $HOME/myexpos/spl/spl_progs/sample_timer.spl
